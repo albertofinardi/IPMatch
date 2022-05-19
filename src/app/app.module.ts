1ee7c7ed66843a348e22e6dba2439be3651221ab
Alberto Finardi <finardialberto@gmail.com>
2022-05-19 16:08:49 -0400

    Bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,8 @@ import { SubnetmaskCalculatorComponent } from './subnetmask-calculator/subnetmas
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirePerformanceModule, PerformanceMonitoringService } from '@angular/fire/compat/performance';
 import { AngularFireAnalyticsModule, ScreenTrackingService } from '@angular/fire/compat/analytics';
+import { environment } from 'src/environments/environment.prod';
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDiGXdmZB1URwEM8GSsxwpsEUn7ykN4NkI",
-  authDomain: "ipmatch.firebaseapp.com",
-  projectId: "ipmatch",
-  storageBucket: "ipmatch.appspot.com",
-  messagingSenderId: "905402177893",
-  appId: "1:905402177893:web:2ddeb32733a298057b298c",
-  measurementId: "G-XCYN7L5HWK"
-};
 
 @NgModule({
   declarations: [
@@ -30,7 +21,7 @@ const firebaseConfig = {
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirePerformanceModule,
     AngularFireAnalyticsModule
   ],
